01b42988ca8ff2ba91d3aec126a85ef32a532bcb
Michael Weikman <michaelweikman@gmail.com>
2013-04-19 19:42:42 -0500

    Version_f no single file handling

--- myls_src/myls.c
+++ myls_src/myls.c
@@ -3,7 +3,6 @@
 
 #include <stdlib.h>
 #include <stdio.h>
-#include <stdio.h>
 #include <getopt.h>
 #include <dirent.h>
 #include <err.h>
@@ -12,89 +11,23 @@
 #include <pwd.h>
 #include <grp.h>
 #include <locale.h>
-#include <time.h>
 #include <math.h>
 #include <string.h>
 
-struct fsInfo{
-     int total_blocksize;
-     int max_file_size;
-     int max_hardlinks;
-     int max_user_name;
-     int max_group_name;
- };
-int numLen(long int y){
+#include "myls.h"
 
-    int i = 0;
-    long int x;
-    x = y;
 
-    if(x >= 100000000){
-        i += 8;
-        x /= 100000000;
-    }
-    if(x >= 1000000){
-        i += 6;
-        x /= 1000000;
-    }
-    if(x >= 10000){
-        i += 4;
-        x /= 10000;
-    }
-    if(x >= 1000){
-        i += 3;
-        x /= 1000;
-    }
-    if(x >= 100){
-        i += 2;
-        x /= 100;
-    }
-    if(x >= 1){
-        i++;;
-    }
-    return i;
-}
-char const * sperm(mode_t mode);
-static void printtime(time_t ftime)
-{
-	int i;
-	char *longstring;
+static void display(char *);
 
-	longstring = ctime(&ftime);
-	for (i = 4; i < 11; ++i)
-		(void)putchar(longstring[i]);
-
-#define	SIXMONTHS	((365 / 2) * (24 * 60 * 60))
-	if (ftime + SIXMONTHS > time(NULL))
-		for (i = 11; i < 16; ++i)
-			(void)putchar(longstring[i]);
-	else {
-		(void)putchar(' ');
-		for (i = 20; i < 24; ++i)
-			(void)putchar(longstring[i]);
-	}
-	(void)putchar(' ');
-}
-static int filter(const struct dirent *d){
-    if((strcmp(d->d_name, ".") == 0) || (strcmp(d->d_name, "..") == 0))
-        return 0;
-    else return 1;
-
-}
-void display(char *);
-void printSCol(struct dirent **, struct stat *, struct fsInfo fsi, int entries);
-void printCol(struct dirent **, int, int);
-
-long blocksize;
 int termwidth = 80; /*Default terminal width*/
-/*Flags*/
+
+/*Flag*/
 int column_flag; /*Column format flag*/
 
 int main(int argc, char *argv[]){
     char *curDir = "./";
     struct winsize win;
     int opt;
-    extern int optind;
 
     if(isatty(STDOUT_FILENO)){
         if(ioctl(STDOUT_FILENO, TIOCGWINSZ, &win) == 0 && win.ws_col > 0)
@@ -110,6 +43,7 @@ int main(int argc, char *argv[]){
                 column_flag = 0;
                 break;
             case '?':
+                usage();
                 break;
             }
         }
@@ -122,133 +56,76 @@ int main(int argc, char *argv[]){
     return 0;
 }
 
-
-void display(char *argv){
+static void display(char *argv){
+    int entries, i, len, max_name_len;
+    char *path;
     struct dirent **ent;
-    int entries, i;
-
-    if((entries = scandir(argv, &ent, filter, alphasort)) == NULL)
-        err(EXIT_FAILURE, NULL);
-
+    struct stat stat;
+    
+    lstat(argv, &stat);
+    if(stat.st_mode & S_IFDIR){
+        if((entries = scandir(argv, &ent, filter, alphasort)) == -1)
+            err(EXIT_FAILURE, NULL);
+        
+        len = strlen(argv);
+        if(argv[len] != '/'){
+            path = (char *) malloc(len + 1);
+            path[0] = '\0';
+            for(i = 0; i < len; i++)
+                path[i] = argv[i];
+            path[len] = '/';
+            len++;
+        }
+        else{
+            path = argv;
+        }
+    }
+    
     if(column_flag){
-        int len, max_name_len = 0;
         for(i = 0; i < entries; ++i){
-
             if((len=strlen(ent[i]->d_name)) > max_name_len)
                 max_name_len = len;
         }
         printCol(ent, entries, max_name_len);
     }
     else{
-        int max;
+        FINFO fsi;
+        int max, blocksize = 0;
+        char* fullpath;
         struct stat *st;
-        struct fsInfo fsi;
         struct passwd *pw;
         struct group *grp;
-        st = (struct stat*)malloc(entries * sizeof(struct stat));
-        fsi.max_file_size = 0;
         fsi.max_hardlinks = 0;
+        fsi.max_file_size = 0;
         fsi.max_user_name = 0;
         fsi.max_group_name = 0;
-        fsi.total_size = 0;
-        fsi.total_blocksize = 0;
+        st = (struct stat*) malloc(entries * sizeof(struct stat));
         for(i = 0; i < entries; ++i){
-            lstat(ent[i]->d_name, &st[i]);
-            fsi.total_blocksize += st[i].st_blocks;
-            pw = getpwuid(st[i].st_uid);
-            grp = getgrgid(st[i].st_gid);
-            if((max = st[i].st_nlink) > fsi.max_hardlinks)
+            fullpath = (char *) malloc(len + strlen(ent[i]->d_name) + 1);
+            fullpath[0] = '\0';
+            strcat(fullpath, path);
+            strcat(fullpath, ent[i]->d_name);
+            lstat(fullpath, &st[i]);
+            if((max = numLen((long int)st[i].st_nlink)) > fsi.max_hardlinks)
                 fsi.max_hardlinks = max;
             if((max = numLen((long int)st[i].st_size)) > fsi.max_file_size)
                 fsi.max_file_size = max;
+            pw = getpwuid(st[i].st_uid);
             if((max = strlen(pw->pw_name)) > fsi.max_user_name)
                 fsi.max_user_name = max;
+            grp = getgrgid(st[i].st_gid);
             if((max = strlen(grp->gr_name)) > fsi.max_group_name)
                 fsi.max_group_name = max;
+            blocksize += st[i].st_blocks;
         }
-        printSCol(ent, st, fsi, entries);
+        fsi.ent = ent;
+        fsi.stat = st;
+        fsi.total_blocksize = blocksize;
+        printSCol(fsi, entries);
         free(st);
+        free(path);
+        free(fullpath);
     }
-}
-
-void printSCol(struct dirent **ent, struct stat *st, struct fsInfo fsi, int entries){
-    int row;
-    struct passwd *pw;
-    struct group *grp;
-
-    printf("total %ld\n", fsi.total_blocksize);
-    for(row = 0; row < entries; row++){
-        printf("%-10.10s ", sperm(st[row].st_mode));
-        printf("%*d ", fsi.max_hardlinks, st[row].st_nlink);
-        pw = getpwuid(st[row].st_uid);
-        printf("%*s  ", fsi.max_user_name, pw->pw_name);
-        grp = getgrgid(st[row].st_gid);
-        printf("%*s  ", fsi.max_group_name, grp->gr_name);
-        printf("%*ld ", fsi.max_file_size,(long int)st[row].st_size);
-        printtime(st[row].st_mtime);
-        printf("%s\n", ent[row]->d_name);
-    }
-}
-
-void printCol(struct dirent **ent, int entries, int max_name_len){
-    int numcols, numrows, row, col, colwidth, base, charCount;
-
-    colwidth = (int)(log((double)max_name_len) / log(2));
-    colwidth = 8 * (colwidth - 1);
-    numcols = (termwidth + colwidth) / colwidth;
-    numrows = entries / numcols;
-    if(numrows % numcols || numrows == 0)
-        numrows++;
-
-    for(row = 0; row < numrows; row++){
-        for(base = row, col = 0;;){
-            charCount =  printf("%s", ent[base]->d_name);
-            if((base += numrows) >= entries) break;
-            if(++col == numcols) break;
-            while(charCount++ < colwidth) (void)putchar(' ');
-
-        }
-        putchar('\n');
-    }
-
-}
-
-char const * sperm(mode_t mode) {
-    static char local_buff[17] = {0};
-    int i = 0;
-    if((mode & S_IFDIR) == S_IFDIR) local_buff[i] = 'd';
-    else local_buff[i] = '-';
-    i++;
-    // user permissions
-    if ((mode & S_IRUSR) == S_IRUSR) local_buff[i] = 'r';
-    else local_buff[i] = '-';
-    i++;
-    if ((mode & S_IWUSR) == S_IWUSR) local_buff[i] = 'w';
-    else local_buff[i] = '-';
-    i++;
-    if ((mode & S_IXUSR) == S_IXUSR) local_buff[i] = 'x';
-    else local_buff[i] = '-';
-    i++;
-    // group permissions
-    if ((mode & S_IRGRP) == S_IRGRP) local_buff[i] = 'r';
-    else local_buff[i] = '-';
-    i++;
-    if ((mode & S_IWGRP) == S_IWGRP) local_buff[i] = 'w';
-    else local_buff[i] = '-';
-    i++;
-    if ((mode & S_IXGRP) == S_IXGRP) local_buff[i] = 'x';
-    else local_buff[i] = '-';
-    i++;
-    // other permissions
-    if ((mode & S_IROTH) == S_IROTH) local_buff[i] = 'r';
-    else local_buff[i] = '-';
-    i++;
-    if ((mode & S_IWOTH) == S_IWOTH) local_buff[i] = 'w';
-    else local_buff[i] = '-';
-    i++;
-    if ((mode & S_IXOTH) == S_IXOTH) local_buff[i] = 'x';
-    else local_buff[i] = '-';
-    return local_buff;
 }
 
 
