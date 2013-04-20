84be611a7ad12b68e0634010ea0d28b3aefdd6f6
Matt Wallace <mattw@cs.txstate.edu>
2013-04-20 15:41:24 -0500

    redirection almost done
    The redirection is almost done.  The command is correcty parsed, the
    actual redirection just doesn't get executed.

--- mysh_src/mysh.c
+++ mysh_src/mysh.c
@@ -25,9 +25,14 @@ char* getPrompt(char**);
 char* getCommand(char**);
 int countDelims(char*, char*);
 void splitLine(char*, char**, const char);
-int exec(char**, char**, char*);
+int exec(char*, char**, char*);
+void getRedirections(char*, char **, char **, char **);
 int processPipes(char*);
 char* resolveSubshells(const char*);
+
+typedef enum {
+	IN, OUT, ERR
+} redirect;
 
 /*
 int execRedirect(char** command, char* input, char* output) {
@@ -213,29 +218,45 @@ void splitLine(char* line, char** result, const char delim) {
     }
 }
 
-int exec(char** command, char** result, char* inputStr) {
+char* glueStrings(char** glued, char** strings, int count) {
+
+	// get the length of the command
+	size_t len = 0;
+	for (int i = 1; i < count; ++i)
+		len += strlen(strings[i]);
+
+	// glue the strings together
+	*glued  = malloc(sizeof(char) * len + 1);
+	**glued = 0;
+	for (int i = 1; i < count; ++i)
+		strcat(*glued, strings[i]);
+}
+
+int exec(char* comm, char** result, char* inputStr) {
 	// backup stdin and stdout
 	int real_stdin  = dup(STDIN);
 	int real_stdout = dup(STDOUT);
 
+	// get reirections for the command
+	char* input=0, *output=0, *error = 0;
+	getRedirections(comm, &input, &output, &error);
+fprintf(stderr, "\tinput: %s\n\toutput: %s\n\terror: %s\n", input, output, error);
+
+	// TODO: perform the actual redirections, the three strings must be freed also
+
+	char** command = malloc(sizeof(char) * (countDelims(comm, " ") + 1));
+	splitLine(comm, command, ' ');
+
+
 	// pipe to capture results
 	int pfd[2];
 	int pfd2[2];
-	int status[2];  // communication between child and parent
+	int status;
 	int err;
 	pid_t cpid;
 
 	if (pipe(pfd) == -1) {
 		perror("exec: pipe");
-		return -1;
-	}
-
-	if (pipe(status) == -1) {
-		perror("exec: pipe");
-		return -1;
-	}
-	if (fcntl(status[1], F_SETFD, fcntl(status[1], F_GETFD) | FD_CLOEXEC)) {
-		perror("fcntl");
 		return -1;
 	}
 
@@ -261,28 +282,27 @@ int exec(char** command, char** result, char* inputStr) {
 		dup2(pfd[1], STDOUT);
 		close(pfd[0]);
 		close(pfd[1]);
-		close(status[0]);
+		//close(status[0]);
 		execvp(*command, command);
 
-		// exec failed
+		// exec failed, cleanup
 		fprintf(stderr, "exec: failed to execute \"%s\"\n", *command);
-		write(status[1], &errno, sizeof(int));
 		dup2(real_stdin, STDIN);
 		dup2(real_stdout, STDOUT);
+
+		// return -1, _exit used instead of exit to ensure child does no
+		// cleanup that should be done by the parent
 		_exit(-1);
 	}
 
 	if (cpid > 0) {
-
-		// check for a failure in the child
-		close(status[1]);
-		while (read(status[0], &err, sizeof(errno)) > 0) {
-			return -1;
-		}
-
 		// wait for child and restore stdout
-		int status = waitpid(cpid, 0, 0);
+		waitpid(cpid, &status, 0);
 		dup2(real_stdout, 1);
+
+		// return if child failed
+		if (status != 0)
+			return -1;
 
 		// create the results buffer
 		*result = malloc(sizeof(char) * (LARGE_BUF + 1));
@@ -303,15 +323,70 @@ int exec(char** command, char** result, char* inputStr) {
 	close(real_stdout);
 }
 
-void getRedirections(char* command) {
-	char *incur, *outcur;
-	char input[FILENAME_MAX]  = "stdin";
-	char output[FILENAME_MAX] = "stdout";
+void getRedirections(char* command, char **input, char **output, char **error) {
+	redirect selected;
+	char *incur, *outcur, *errcur;
+	char target[FILENAME_MAX];
+
+	*input = 0;
+	*output = 0;
+	*error = 0;
 
 	// while some redirection still exists
-	while ((incur = strstr(command, "<")) || (outcur = strstr(command, ">"))) {
+	incur  = strstr(command, "<");
+	outcur = strstr(command, ">");
+	errcur = strstr(command, "2>");
 
+	while (incur || outcur|| errcur) {
+		// find the closest to the start
+		char* cursor = (void *)-1;
 
+		if (incur != 0)
+			cursor = incur;
+		if (outcur != 0 && outcur < cursor)
+			cursor = outcur;
+		if (errcur != 0 && errcur < cursor)
+			cursor = errcur;
+
+		// set the redirect type
+		if (cursor == incur)
+			selected = IN;
+		else if (cursor == outcur)
+			selected = OUT;
+		else
+			selected = ERR;
+
+		// mark start of delete
+		char* mark1 = cursor;
+
+		// grab the value
+		cursor += (selected == ERR) ? 2 : 1;
+		cursor += (*cursor == ' ') ? 1 : 0;
+		sscanf(cursor, "%s", target);
+
+		// store the value
+		if (selected == IN) {
+			if (*input)
+				free(*input);
+			*input = strdup(target);
+		}
+		else if (selected == OUT) {
+			if (*output)
+				free(*output);
+			*output = strdup(target);
+		}
+		else if (selected == ERR) {
+			if (*error)
+				free(*error);
+			*error = strdup(target);
+		}
+
+		cursor += strlen(target);
+		memcpy(mark1, cursor, strlen(cursor)+1);
+
+		incur  = strstr(command, "<");
+		outcur = strstr(command, ">");
+		errcur = strstr(command, "2>");
 	}
 }
 
@@ -324,20 +399,17 @@ int processPipes(char* lineRead) {
 
 	for (int i = 0; pipes && pipes[i]; ++i) {
 		pipes[i]       = resolveSubshells(pipes[i]);
-		// TODO: handle redirections here
-		char** command = malloc(sizeof(char) * (countDelims(pipes[i], " ") + 1));
-		splitLine(pipes[i], command, ' ');
 
 		// check for an exit
 		if (strcmp("exit", trim(pipes[i])) == 0)
 			exit(EXIT_SUCCESS);
 
 		if (i == 0) {              // first command
-			status = exec(command, &last, 0);
+			status = exec(pipes[i], &last, 0);
 		}
 		else {                     // not first command
 			char* temp = 0;
-			status = exec(command, &temp, last);
+			status = exec(pipes[i], &temp, last);
 			free(last);
 			last = temp;
 		}
@@ -369,11 +441,8 @@ char* resolveSubshells(const char* line) {
 
 		size_t subSize = strlen(subcmd);
 
-		char** parsed = malloc(sizeof(char) * (countDelims(subcmd, WHITESPACE) + 1));
-		splitLine(subcmd, parsed, ' ');
 		char*  subshellDump = 0;
-
-		exec(parsed, &subshellDump, 0);
+		exec(subcmd, &subshellDump, 0);
 
 		size_t tempSize = (originSize - subSize + strlen(subshellDump));
 		char* temp = malloc(sizeof(char) * (tempSize + 1));
@@ -392,22 +461,17 @@ int main(int argc, char** argv) {
 		return 127;
 	}
 
-	// handle a one-off execution
-	if (argc > 1) {
-		char* result = 0;
-		exec(argv+1, &result, 0);
+	// flag to exit early
+	int more = 1;
 
-		if (result) {
-			fprintf(stdout, "%s\n", result);
-			exit(EXIT_SUCCESS);
-		}
-		else
-			exit(EXIT_FAILURE);
-	}
+	while(more) {            // main terminal loop
+		char* lineRead = { 0 };
 
-	while(1) {            // main terminal loop
-		char* lineRead;
-		getCommand(&lineRead);
+		if (argc > 1) {
+			glueStrings(&lineRead, argv, argc);
+			more = 0;
+		} else
+			getCommand(&lineRead);
 		processPipes(lineRead);
 
 
