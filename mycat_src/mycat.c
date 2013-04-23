b5c6b584e6e61d211fbfe702c923de2b03871f5b
Daniel-Brooks <bldkbrooks@sbcglobal.net>
2013-04-22 19:36:56 -0500

    Implemented mycat

--- mycat_src/mycat.c
+++ mycat_src/mycat.c
@@ -1,61 +1,64 @@
-#define _XOPEN_SOURCE   500
-
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
-#include <string.h>
-#include <sys/types.h>
+#include <stdbool.h>
 #include <sys/stat.h>
-#include <fcntl.h>
 
-#define LARGE_BUFF 4096
-
-int cat(int);
-
-int main(int argc, char** argv) {
-
+int main(int argc, char* argv[])
+{
+	FILE* fp;
+	struct stat fileStat;
+	int i;
+	int c;
+	
 	if (argc == 1)
-		cat(0);
-
-	else {
-		int fd;
-		for (size_t i = 1; i < argc; ++i) {
-			struct stat file;
-
-			// get file information
-			if (stat(argv[i], &file) == -1) {
-				perror("mycat");
+	{
+		size_t size = 256;
+		char* str = (char*) malloc(size);
+		int count;
+		while (true)
+		{
+			count = getline(&str, &size, stdin);
+			if (count == -1)
 				break;
-			}
-
-			// print error and skip if a directory
-			if ((file.st_mode & S_IFMT) == S_IFDIR) {
-				fprintf(stderr, "mycat: %s is a directory\n", argv[i]);
-				continue;
-			}
-
-			if ((fd = open(argv[i], O_RDONLY)) == -1) {
-				fprintf(stderr, "Could not open file %s", argv[i]);
-				break;
-			}
-			if (cat(fd) < 0) {
-				fprintf(stderr, "Error reading file: %s\n", argv[i]);
-			}
-			close(fd);
+			printf("%s", str);
 		}
+		free(str);
+		return EXIT_SUCCESS;
 	}
+	
+	for (i = 1; i < argc; i++)
+	{
+		if(stat(argv[i], &fileStat) < 0)
+		{
+			printf("mycat: %s: The file failed to open\n", argv[i]);
+			continue;
+		}
+		
+		if (S_ISDIR(fileStat.st_mode))
+		{
+			printf("mycat: %s: The file is a directory\n", argv[i]);
+			continue;
+		}
+		
+		fp = fopen(argv[i], "r");
+		if (fp == NULL)
+		{
+			printf("mycat: %s: No such file or directory.\n", argv[i]);
+			continue;
+		}
 
-	return 0;
-}
-
-int cat(int fd) {
-	char buf[LARGE_BUFF];
-	size_t bytesRead = 0;
-
-	while((bytesRead = read(fd, buf, LARGE_BUFF - 1)) > 0) {
-		if (write(1, buf, bytesRead) < 0)
-			perror("stdout");
+		while (c = fgetc(fp))
+		{
+			if (c == EOF)
+				break;
+			printf("%c", c);
+		}
+		
+		
+		fclose(fp);
 	}
-
-	return bytesRead;
+	
+	
+	return EXIT_SUCCESS;
 }
